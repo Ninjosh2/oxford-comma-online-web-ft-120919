de1072948ea8d546baf3eea4375afe8834db5ed6
Joshua Sanders <ninjosh2@gmail.com>
2019-12-12 21:39:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
      array[0..1].join (" and ")
      
    else 
-     array[0...-1].join(", ") << ", and #{array[array.length > 3]}"
+     array[0...-1].join(", ") << ", and #{array[-1]}"
    end
    
    
