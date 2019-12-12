1d2b54d25be835a85e57bb9334162b2a9f278b4f
Joshua Sanders <ninjosh2@gmail.com>
2019-12-12 18:50:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,16 @@
 def oxford_comma(array)
  case array.length 
-when 1
+ 
+  when 1
   "#{array[0]}"
-when 2
+  
+  when 2
   array[0..1].join(" and ")
-else 
+  
+  else 
   array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
+  end 
+  
 end
+
 
